1ff5db9e7470adb03a9d2ba6398575a778eb9455
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-04-21 20:52:50 +0530

    Establishing the basic routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { Route, Routes } from 'react-router';
 import Home from './components/home/home.component';
 import Genre from './components/genre/genre.component';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Genre />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="genre" element={<Genre />} />
+    </Routes>
   );
 }
 
