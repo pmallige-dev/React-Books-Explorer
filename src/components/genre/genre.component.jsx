1341eb7dcb96cf84b7e4d0326d085257bb3aa7a7
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-04-23 19:55:38 +0530

    Implemented the infinite scroll feature - Using window event

--- src/components/genre/genre.component.jsx
+++ src/components/genre/genre.component.jsx
@@ -1,14 +1,21 @@
-import { useState, useCallback, useEffect, useRef, useContext } from "react";
+import { useContext } from "react";
 import SearchBox from "../search-box/search-box.component";
 import BooksList from "../books-list/books-list.component";
 import { BooksContext } from "../../context/books.context";
 
 const Genre = () => {
+
+    const { isLoading } = useContext(BooksContext)
+
     return (
         <div className="genre-container">
             <SearchBox />
             <BooksList />
-            <div />
+            {isLoading && (
+                <div className="loading-new-books">
+                    Loading New Books ...
+                </div>
+            )}
         </div>
     )
 }
--- src/context/books.context.jsx
+++ src/context/books.context.jsx
@@ -3,6 +3,10 @@ import { createContext, useState, useEffect } from "react";
 export const BooksContext = createContext({
     page: 1,
     setPage: () => null,
+    isFirstLoad: true,
+    setIsFirstLoad: () => null,
+    isLoading: true,
+    setIsLoading: () => null,
     searchField: '',
     setSearchField: () => null,
     bookList: [],
@@ -14,19 +18,47 @@ export const BooksContext = createContext({
 export const BooksProvider = ({ children }) => {
 
     const [page, setPage] = useState(1);
+    const [isFirstLoad, setIsFirstLoad] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [searchField, setSearchField] = useState('');
     const [bookList, setBookList] = useState([]);
     const [filteredBookList, setFilteredBookList] = useState(bookList);
 
     useEffect(() => {
-        const fetchBooks = async () => {
-            const fetchUrl = await fetch(`http://skunkworks.ignitesol.com:8000/books/?page=${page}`);
-            const response = await fetchUrl.json();
-            const bookResults = await response.results;
-            setBookList(bookResults);
-        };
         fetchBooks();
-    }, [setPage, page]);
+        window.addEventListener("scroll", handleScroll);
+    }, [page]);
+
+    const fetchBooks = async () => {
+        const bookResults = await fetchingBooksFromAPI(page);
+        storingAPIDataInSession(bookResults, setBookList);
+        setIsFirstLoad(false);
+        setIsLoading(false);
+    };
+
+    const storingAPIDataInSession = (bookResults, setBookList) => {
+        window.sessionStorage.setItem(`bookResultsSessionPage${page}`, JSON.stringify(bookResults));
+        const newGetSessionStoredAPIData = JSON.parse(window.sessionStorage.getItem(`bookResultsSessionPage${page}`)) || [];
+        if (bookList.length == 0) {
+            setBookList(newGetSessionStoredAPIData);
+        }
+        else {
+            setBookList(bookList => bookList.concat(newGetSessionStoredAPIData));
+        }
+    }
+
+    const fetchingBooksFromAPI = async (page) => {
+        const fetchUrl = await fetch(`http://skunkworks.ignitesol.com:8000/books/?page=${page}`);
+        const response = await fetchUrl.json();
+        const bookResults = await response.results;
+        return bookResults;
+    }
+
+    const handleScroll = () => {
+        if (Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.offsetHeight) {
+            setPage(page + 1);
+        }
+    };
 
     useEffect(() => {
         const NewFilteredBookList = bookList.filter((book) => {
@@ -41,12 +73,6 @@ export const BooksProvider = ({ children }) => {
         setSearchField(searchFieldString);
     };
 
-    const onInfiniteScroll = (event) => {
-        const pageNumber = page;
-        if (event)
-            setPage(pageNumber++)
-    }
-
     const value = {
         page,
         setPage,
@@ -56,7 +82,8 @@ export const BooksProvider = ({ children }) => {
         setBookList,
         filteredBookList,
         setFilteredBookList,
-        onSearchChange
+        onSearchChange,
+        isLoading
     }
 
     return <BooksContext.Provider value={value}>{children}</BooksContext.Provider>
